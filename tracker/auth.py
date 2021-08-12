3be270f7cc029fc4e349a196afd6cb959b7e8d8c
Trevor <trevt.sd@gmail.com>
2021-08-11 23:56:01 -0700

    auth.py

--- tracker/auth.py
+++ tracker/auth.py
@@ -1,5 +1,5 @@
 import spotipy
-from spotify.oauth2 import SpotifyOAuth
+from spotipy.oauth2 import SpotifyOAuth
 
 CLIENT_ID = '670c083902ee4f6e9ded059a85e9dc6d'
 CLIENT_SECRET = '711293c9db644ac1b8adbb9ed0cf4125'
@@ -9,8 +9,11 @@ REDIRECT_URL = 'http://127.0.0.1:8000/'
 SCOPES = 'user-library-read'
 
 url = SPOTIFY_AUTHORIZE #Add base auth url
-url = url + '?client_id=' + CLIENT_ID #Add client ID
-url = url + '&response_type=code'
-url = url + '&redirect_uri=' + REDIRECT_URL
-url = url + '&show_dialog=true'
-url = url + '&scope=' + SCOPES
+url += '?client_id=' + CLIENT_ID #Add client ID
+url += '&response_type=code'
+url += '&redirect_uri=' + REDIRECT_URL
+url += '&show_dialog=true'
+url += '&scope=' + SCOPES
+
+def getCode():
+    
--- tracker/views.py
+++ tracker/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .auth import *
 
 # Create your views here.
 def home(request):
