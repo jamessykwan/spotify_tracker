1e7fee2c1dd362fa89fa0eeb04f0c43ac93c1a07
Trevor <trevt.sd@gmail.com>
2021-08-16 22:59:48 -0700

    tokens

--- tracker/auth.py
+++ tracker/auth.py
@@ -1,12 +1,14 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+from requests import Request, post
+
 
 CLIENT_ID = '670c083902ee4f6e9ded059a85e9dc6d'
 CLIENT_SECRET = '711293c9db644ac1b8adbb9ed0cf4125'
 SPOTIFY_AUTHORIZE = 'https://accounts.spotify.com/authorize'
 REDIRECT_URL = 'http://127.0.0.1:8000/'
 
-SCOPES = 'user-library-read'
+SCOPES = 'playlist-read-private'
 
 auth_url = SPOTIFY_AUTHORIZE #Add base auth url
 auth_url += '?client_id=' + CLIENT_ID #Add client ID
@@ -15,7 +17,28 @@ auth_url += '&redirect_uri=' + REDIRECT_URL
 auth_url += '&show_dialog=true'
 auth_url += '&scope=' + SCOPES
 
-print(auth_url)
+def create_oauth():
+    return SpotifyOAuth(
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET,
+        redirect_uri=auth_url,
+        scope=SCOPES
+    )
 
-def getCode():
-    return
+def get_tokens(request, format=None):
+    code = request.GET.get('code')
+    error = request.GET.get('error')
+
+    response = post('https://accounts.spotify.com/api/token',data={
+        'grant_type': 'authorization_code',
+        'code': code,
+        'redirect_uri': REDIRECT_URL,
+        'client_id': CLIENT_ID,
+        'client_secret': CLIENT_SECRET
+    }).json()
+
+    access_token = response.get('access_token')
+    token_type = response.get('token-type')
+    refresh_token = response.get('refresh_token')
+    expires_in = response.get('expires_in')
+    error = response.get('error')
